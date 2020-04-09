098c86664fd32585371fff08124c75ec21141249
myGanter <4el0073@mail.ru>
2020-04-09 21:40:16 +0300

    Added serialization methods and basic library exception types

---
+++ src/NodeCore/NodeCore/Realization/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeCore.Realization
+{
+    public class NodeCoreEx : Exception 
+    { }
+
+    public class GraphEx : NodeCoreEx
+    { }
+
+    public class ProcessorEx : NodeCoreEx
+    { }
+}
---
+++ src/NodeCore/NodeCore/Realization/Serialization/GraphSerializationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NodeCore.Base;
+using System.IO;
+
+namespace NodeCore.Realization.Serialization
+{
+    public static class GraphSerializationExtensions
+    {
+        public static void SerializeToBinary<T>(this IGraph<T> Graph, Stream SerializationStream) 
+        {
+
+        }
+
+        public static byte[] SerializeToBinary<T>(this IGraph<T> Graph)
+        {
+            throw new Exception();
+        }
+
+        public static void SerializeToXml<T>(this IGraph<T> Graph, Stream SerializationStream)
+        {
+
+        }
+
+        public static string SerializeToXml<T>(this IGraph<T> Graph)
+        {
+            throw new Exception();
+        }
+    }
+}
