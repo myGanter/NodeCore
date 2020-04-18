c42a7011a9b6c565be7d333e097682855753a175
myGanter <4el0073@mail.ru>
2020-04-18 15:35:23 +0300

    Add StreamXmlSerializer

---
+++ src/NodeCore/NodeCore/Realization/Serialization/StreamXmlSerializer.cs
@@ -0,0 +1,29 @@
+﻿using NodeCore.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NodeCore.Realization.Serialization
+{
+    public class StreamXmlSerializer<T> : BaseXmlSerializer<T, Stream>
+    {
+        public StreamXmlSerializer(IGraph<T> Graph, Stream SerializationObj) :
+            base(Graph, SerializationObj, false)
+        { }
+
+        public StreamXmlSerializer(IGraph<T> Graph, Stream SerializationObj, bool SerializeTType, XmlSerializerNamespaces TTypeSerializerNamespaces = null) :
+            base(Graph, SerializationObj, SerializeTType, TTypeSerializerNamespaces)
+        { }
+
+        protected override XmlWriter CreateXmlWriter()
+        {
+            var settings = GetDefaultXmlWriterSettings();
+            var xw = XmlWriter.Create(SerializationObj, settings);
+
+            return xw;
+        }
+    }
+}
